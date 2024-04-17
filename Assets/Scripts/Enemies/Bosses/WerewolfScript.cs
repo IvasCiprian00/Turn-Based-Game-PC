2c7f351a6c8e61d8b8c99812f6a41faeec1c4d78
Ciprian Ivas <ciprian@chirpy.digital>
2024-04-17 12:55:21 +0300

    add werewolf boss

---
+++ Assets/Scripts/Enemies/Bosses/WerewolfScript.cs
@@ -0,0 +1,127 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEditor;
+using UnityEngine;
+
+public class WerewolfScript : Enemy
+{
+    [Serializable]
+    public struct HpThreshold
+    {
+        public int speed;
+        public int damage;
+        public int attackCount;
+    }
+
+    [SerializeField] HpThreshold[] _hpThreshold;
+
+    [Header("Skills")]
+    [SerializeField] private GameObject _swipe;
+    [SerializeField] private int _swipeCooldown;
+    private int _swipeTimer;
+    [SerializeField] private GameObject _rupture;
+    [SerializeField] private int _ruptureCooldown;
+    private int _ruptureTimer;
+
+    public void Awake()
+    {
+        SetManagers();
+        SetHealthbar();
+    }
+
+    public void Start()
+    {
+        _hp = _maxHp;
+
+        _ruptureTimer = _ruptureCooldown;
+        _swipeTimer = _swipeCooldown;
+
+        UpdateHealthbar();
+    }
+
+    public void Update()
+    {
+        Movement();
+    }
+
+    override
+    public void StartTurn()
+    {
+        StartCoroutine(TakeTurn());
+    }
+
+    public IEnumerator TakeTurn()
+    {
+        int speedLeft = _speed;
+        int attacksLeft = _attackCount;
+
+        /*_ruptureTimer++;
+        _swipeTimer++;
+
+        if(_swipeTimer >= _swipeCooldown)
+        {
+            Debug.Log("SWIPE");
+            _swipeTimer = 0;
+        }
+        if(_ruptureTimer >= _ruptureCooldown)
+        {
+            Debug.Log("RUPTURE");
+            _ruptureTimer = 0;
+        }*/
+
+        while (speedLeft > 0 || attacksLeft > 0)
+        {
+            FindTarget();
+
+            if (CanAttack(_heroScript) && attacksLeft == 0)
+            {
+                break;
+            }
+
+            if (!CanAttack(_heroScript) && speedLeft == 0)
+            {
+                break;
+            }
+
+            if (CanAttack(_heroScript))
+            {
+                _heroScript.TakeDamage(_damage);
+                attacksLeft--;
+            }
+            else if (speedLeft > 0)
+            {
+                MoveTowardsTarget();
+                speedLeft--;
+            }
+
+            yield return new WaitForSeconds(0.2f);
+        }
+
+        EndTurn();
+    }
+
+    public new void TakeDamage(int dmg)
+    {
+        base.TakeDamage(dmg);
+        
+        if(_hp <= 0.3 * _maxHp)
+        {
+            ChangeThreshold(1);
+            return;
+        }
+
+        if(_hp <= 0.7 * _maxHp)
+        {
+            ChangeThreshold(0);
+        }
+    }
+
+    public void ChangeThreshold(int x)
+    {
+        _speed = _hpThreshold[x].speed;
+        _damage = _hpThreshold[x].damage;
+        _attackCount = _hpThreshold[x].attackCount;
+    }
+}
