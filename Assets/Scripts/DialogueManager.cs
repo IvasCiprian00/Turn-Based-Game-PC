9d251ae80294a5bde313f936295c6b265c30a054
Ciprian Ivas <ciprian@chirpy.digital>
2024-05-24 14:06:12 +0300

    add basic intro scene

--- Assets/Scripts/DialogueManager.cs
+++ Assets/Scripts/DialogueManager.cs
@@ -1,63 +1,114 @@
+using JetBrains.Annotations;
 using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using TMPro;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class DialogueManager : MonoBehaviour
 {
-    [SerializeField] private TextMeshProUGUI _dialogueText;
-    [SerializeField] private TextAsset _testFile;
-    private string _dialogueLine;
     private string _path;
     private StreamReader _reader;
-    private IEnumerator _dialogueCoroutine;
-    private bool _lineHasFinished;
+    [SerializeField] private Animator _animator;
 
-    private int _index;
+    [Header("Dialogue")]
+    [SerializeField] private RectTransform _containerRect;
+    [SerializeField] private TextMeshProUGUI _dialogueText;
+    private string _dialogueLine;
+
+    [Header("Speakers")]
+    [SerializeField] private GameObject _hero;
+    [SerializeField] private GameObject _elder;
+    [SerializeField] private GameObject _elderSprite;
 
     private void Start()
     {
         _path = "Assets/Dialogue/intro_dialogue.txt";
         _reader = new StreamReader(_path);
-
-        _dialogueLine = _reader.ReadLine();
-        _dialogueText.text = _dialogueLine.Substring(_dialogueLine.IndexOf(":") + 1);
     }
 
     public void Update()
     {
-        if(Input.GetMouseButtonUp(0))
+        _containerRect.sizeDelta = new Vector2(800, _dialogueText.preferredHeight);
+
+        if (Input.GetMouseButtonUp(0))
         {
-            int maxIterations = 0;
-            while(true)
+            GetDialogueLine();
+        }
+    }
+
+    public void SetElderPosition(float yPos)
+    {
+        _elderSprite.transform.position = new Vector2(_elderSprite.transform.position.x, yPos);
+
+        BeginDialogue();
+    }
+
+    public void BeginDialogue()
+    {
+        _containerRect.gameObject.SetActive(true);
+        GetDialogueLine();
+    }
+
+    public void GetDialogueLine()
+    {
+        int maxIterations = 0;
+        while (true)
+        {
+            maxIterations++;
+            _dialogueLine = _reader.ReadLine();
+
+            if (maxIterations >= 5)
             {
-                maxIterations++;
-                _dialogueLine = _reader.ReadLine();
-
-                if(maxIterations >= 5)
-                {
-                    break;
-                }
-
-                if(_reader.EndOfStream)
-                {
-                    break;
-                }
-
-                if(_dialogueLine.IndexOf(":") != -1)
-                {
-                    break;
-                }
-
+                break;
             }
 
-            if(_dialogueLine == null)
+            if (_reader.EndOfStream)
             {
-                return;
+                _containerRect.gameObject.SetActive(false);
+                _animator.SetTrigger("exit intro");
+                break;
             }
-            _dialogueText.text = _dialogueLine.Substring(_dialogueLine.IndexOf(":") + 1);
+
+            if (_dialogueLine.IndexOf(":") != -1)
+            {
+                break;
+            }
+
+        }
+
+        if (_dialogueLine == null)
+        {
+            return;
+        }
+
+        SetSpeaker();
+
+        _dialogueText.text = _dialogueLine.Substring(_dialogueLine.IndexOf(":") + 1);
+    }
+
+    public void ExitIntro()
+    {
+        SceneManager.LoadScene("Camp Screen");
+    }
+
+    public void SetSpeaker()
+    {
+        if(_dialogueLine.IndexOf("hero:") != -1)
+        {
+            //_containerRect.transform.position = new Vector2(_hero.transform.position.x, _hero.transform.position.y + 1);
+            _containerRect.transform.position = _hero.transform.position;
+            Debug.Log(_hero.transform.position);
+
+        }
+
+        if(_dialogueLine.IndexOf("elder:") != -1)
+        {
+            Debug.Log(_elder.transform.position);
+            //_containerRect.transform.position = new Vector2(_elder.transform.position.x, _elder.transform.position.y + 1);
+            _containerRect.transform.position = _elder.transform.position;
         }
     }
 
